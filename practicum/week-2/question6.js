fa0713a196fb6e1397da4717f205974dfdf87881
Abbey Rohler <abbeymr11@gmail.com>
2022-01-31 20:33:57 -0700

    Updated Question 6

--- practicum/week-2/question6.js
+++ practicum/week-2/question6.js
@@ -4,7 +4,17 @@ Example:
 input: [1, 2, 2, 3, 4, 7, 9]
 output: 3 
 */
-
-var median = function(input) {
-
-};
+function funct(n){
+    let mid = n.sort().length;
+    let midsort= n.sort();
+    if (mid%2 == 0){
+   var  mid1 = mid/2;
+  	ar = (midsort[mid1] + midsort[mid1-1])/2;
+    return ar;
+        }
+      else{
+        var mid1 = Math.floor(mid/2)
+        return midsort[mid1];
+      }
+        }
+console.log(funct([1,2,2,3,4,7,9]));
