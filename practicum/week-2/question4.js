c237e81c6ed51156204afcc2e2b2b3fb451afd56
Abbey Rohler <abbeymr11@gmail.com>
2022-01-31 19:22:18 -0700

    test2

--- practicum/week-2/question4.js
+++ practicum/week-2/question4.js
@@ -9,7 +9,19 @@ The letters in A are guaranteed distinct, and all characters in A and B are lett
  input: A='gtY', B = 'iTygrtg'
  output: 3
 */
-
-var howManyCommon = function(A, B) {
-
-};
+function funct(A,B){
+    const str = A;
+    const str2 = B;
+    let A1 = str.toLowerCase();
+    let B1 = str2.toLowerCase();
+    let count = 0;
+    for (let c of str){
+    	for (let d of str2){
+      if (c == d){
+      count++;
+      
+      }
+      }
+    }return count;
+    }
+ console.log(funct('asDf','dsssaaFa'));
